f4f32ab46087f770efa582b0babf0a9a6b357fd2
arthurderuelle <aderuelle@studiosystems.fr>
2026-07-21 12:12:31 +0200

    prettier config

---
+++ Desktop/asmajob/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = {
+    title: "Login",
+    description: "Login to your account",
+}
+
+const Login = () => {
+    return <div>Login
+    </div>;
+}
+
+export default Login;
--- Desktop/asmajob/app/page.tsx
+++ Desktop/asmajob/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="bg-red-500 ">
-ok
+    <div className="">
+      home page
+      <p>jghj</p>
     </div>
   );
 }
